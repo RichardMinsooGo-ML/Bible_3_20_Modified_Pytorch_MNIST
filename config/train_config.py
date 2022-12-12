b606d77dd268029ce41ac946dcbb627c4a4194ee
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-12-13 07:10:35 +0900

    Add files via upload

--- config/train_config.py
+++ config/train_config.py
@@ -8,13 +8,15 @@ def parse_train_configs():
     parser = argparse.ArgumentParser(description='The Implementation of Pyorch YOLOv3')
     
     # parser.add_argument("--data_config" , type=str,   default="config/custom.data", help="path to data config file")
-    parser.add_argument("--working-dir"  , type=str, default='./', metavar='PATH', help='The ROOT working directory')
-    parser.add_argument("--input_size"   , type=int, default=784, help="input_size 28x28")
-    parser.add_argument("--hidden_size"  , type=int, default=200, help="hidden_size")
-    parser.add_argument("--output_dim"   , type=int, default=10, help="size of each image dimension")
-    parser.add_argument("--num_epochs"   , type=int, default=30, help="number of epochs")
-    parser.add_argument("--batch_size"   , type=int, default=100, help="size of each image batch")
+    parser.add_argument("--working-dir" , type=str, default='./', metavar='PATH', help='The ROOT working directory')
+    parser.add_argument("--input_size"  , type=int, default=784, help="input_size 28x28")
+    parser.add_argument("--hidden_size" , type=int, default=200, help="hidden_size")
+    parser.add_argument("--output_dim"  , type=int, default=10, help="size of each image dimension")
+    parser.add_argument("--num_epochs"  , type=int, default=2, help="number of epochs")
+    parser.add_argument("--batch_size"  , type=int, default=4, help="size of each image batch")
     parser.add_argument('--learning_rate', type=float, default=0.0005, help='learning_rate')
+    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+
     configs = edict(vars(parser.parse_args()))
     
     ############## Dataset, logs, Checkpoints dir ######################
